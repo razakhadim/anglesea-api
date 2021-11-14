be5023419d09282709e03a4c1a33b92ac22eceab
Raza Khadim <razakhadim@yahoo.com>
2021-11-14 13:46:54 +1300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0685b3062c6474aa5012d760b4ecbeb0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96d7ba4a-2b62-4b3f-bb12-7ad954930de2'
+};
